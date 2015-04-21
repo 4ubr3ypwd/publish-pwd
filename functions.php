6e69f09593518f3367b5413a7112edfe89447b30
Aubrey Portwood <aubreypwd@gmail.com>
2015-04-20 22:12:32 -0700

    Class based function file.

--- functions.php
+++ functions.php
@@ -3,3 +3,18 @@
 if ( ! isset( $content_width ) ) {
 	$content_width = 852; /* pixels */
 }
+
+class Publish_PWD {
+	function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles') );
+	}
+
+	/**
+	 * Enqueues the parent theme's CSS.
+	 */
+	function enqueue_styles() {
+		wp_enqueue_style( 'publish-pwd', get_template_directory_uri() . '/style.css' );
+	}
+}
+
+new Publish_PWD();
